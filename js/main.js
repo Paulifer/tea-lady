4ee3dec79de96add8963958fa91d9cbb3da1b5a2
paulina huañaco <pauli.hu.timm@gmail.com>
2017-05-17 16:27:04 -0400

    arregle la funcion para imprimir pero no funciono. Agregue diseño en css

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 function mostarData(){
 	var nombre = document.getElementById("nombre").value;
     var apelido = document.getElementById("apellido").value;
-    var correo = document.getElementById("email).value";
+    var correo = document.getElementById("email").value;
     var direccion = document.getElementById("direccion").value;
     var telefono = document.getElementById("telefono").value;
-    var seleciona = document.getElementById("selector").seleaIndex;
+    var seleciona = document.getElementById("selector").value;
  	
- 	if(nombre === "" || apelido === "" || correo === "" || direccion === "" || telefono === "" ){
-			alert("Todos los campos son obligatorios")
-			return;
+ 	if(nombre == null || apelido == null || correo == null || direccion == null || telefono == null ){
+			alert("Todos los campos son obligatorios");
+			return true;
 		}
-	else if ( !selector.checked ) {
-  return false;
-		}
+
 }
 
